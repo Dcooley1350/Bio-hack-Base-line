a06625d38d31c8436514ee5153c702e1db393d42
Student <example@example.com>
2019-12-11 09:25:42 -0800

    Add redux store and provider to App component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,22 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Application from './components/Application';
+import Provider from 'react-redux';
+import rootReducer from './reducers/index';
+import { createStore } from 'redux';
+
+const store = createStore(rootReducer);
+
+let unsubscribe = store.subscribe(() => 
+console.log(store.getState())
+);
+
 
 function App() {
   return (
-    <Application/>
+    <Provider store={store}>
+      <Application/>
+    </Provider>
   );
 }
 
