e1d7be323a601fb39e915e50ef5826fbc1601958
Student <example@example.com>
2019-12-11 10:27:25 -0800

    Write newUser thunk

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,3 +3,13 @@ import firebase from 'firebase';
 const { firebaseConfig } = constants;
 
 firebase.initializeApp(firebaseConfig);
+
+const users = firebase.database().ref('users');
+
+export function newUser(_firstName, _lastName, _email, _password) {
+    return () => tickets.push({
+        firstName: _firstName,
+        lastName: _lastName,
+        email: 
+    })
+}
