5a4f1762c444dc3234381ea65853a178b165bcfa
Student <example@example.com>
2019-12-11 15:50:09 -0800

    Write currentTestReducer

---
+++ src/reducers/currentTestReducer.js
@@ -0,0 +1,15 @@
+import c from './../constants';
+
+const currentTestReducer = (state = {}, action) => {
+    let newState;
+    switch(action.type){
+        case c.NEW_CURRENT_TEST:
+            newState= Object.assign({}, state, action.currentTest)
+            return newState;
+
+            default:
+            return state;
+    }
+}
+
+export default currentTestReducer;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import userReducer from './userReducer';
 import testReducer from './testReducer';
 import { combineReducers } from 'redux';
+import currentTestReducer from './currentTestReducer';
 
 const rootReducer = combineReducers({
     currentUser: userReducer,
     testsById: testReducer,
+    currentTest: currentTestReducer,
 })
 
 export default rootReducer;
