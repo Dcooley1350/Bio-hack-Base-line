ee2dd6a4d6bf1c78f2c72a29a8e4923d83550d71
Student <example@example.com>
2019-12-12 14:17:02 -0800

    Refactor Sidebar to show test results during test

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -3,33 +3,76 @@ import { connect } from 'react-redux';
 
 const SideNav = (props) => {
   let greeting = null;
-  const sideNavStyle= {
+  let content = null;
+
+  const sideNavStyle = {
     height: '100vh',
     backgroundColor: '#ececeb',
     width: '100%'
   };
 
-  const greetingStyle={
+  const greetingStyle = {
     textAlign: 'center',
   }
+  function sideNavConditionalRender() {
+    if (props.currentRouterPath === '/Archive') {
+      greeting = 'Archive';
 
-  if(props.currentRouterPath === '/Archive'){
-    greeting ='Archive';
+    }
+    else if (props.currentRouterPath === '/Test') {
+      greeting = 'Test';
+      console.log(props.currentTest);
+      if (props.currentTest == '')
+         content = <p>Test data will appear here as test progresses.</p>
+      else {
+        content = 
+          <div>
+            <h5>Info:</h5>
+            <ul>
+              <li>{props.currentTest.expCondition}</li>
+              <li>{props.currentTest.ctrlCondition}</li>
+              <li>Start Time: {props.currentTest.time}</li>
+              <li>Date: {props.currentTest.date}</li>
+            </ul>
+            <h5>Reaction Time Tests:</h5>
+            <ul>
+              {props.currentTest.ReactionTimeTest1 ? (<li>Test 1: {props.currentTest.ReactionTimeTest1}</li>) : (<li>Test 1: </li>)}
+              {props.currentTest.ReactionTimeTest2 ? (<li>Test 2: {props.currentTest.ReactionTimeTest2}</li>) : (<li>Test 2: </li>)}
+              {props.currentTest.ReactionTimeTest3 ? (<li>Test 3: {props.currentTest.ReactionTimeTest3}</li>) : (<li>Test 3: </li>)}
+              {props.currentTest.ReactionTimeTest4 ? (<li>Test 4: {props.currentTest.ReactionTimeTest4}</li>) : (<li>Test 4: </li>)}
+              {props.currentTest.ReactionTimeTest5 ? (<li>Test 5: {props.currentTest.ReactionTimeTest5}</li>) : (<li>Test 5: </li>)}
+            </ul>
+            <h5>Memory Tests:</h5>
+            <ul>
+              {props.currentTest.MemoryTest1 ? (<li>Test 1: {props.currentTest.MemoryTest1}</li>) : (<li>Test 1: </li>)}
+              {props.currentTest.MemoryTest2 ? (<li>Test 2: {props.currentTest.MemoryTest2}</li>) : (<li>Test 2: </li>)}
+              {props.currentTest.MemoryTest3 ? (<li>Test 3: {props.currentTest.MemoryTest3}</li>) : (<li>Test 3: </li>)}
+              {props.currentTest.MemoryTest4 ? (<li>Test 4: {props.currentTest.MemoryTest4}</li>) : (<li>Test 4: </li>)}
+              {props.currentTest.MemoryTest5 ? (<li>Test 5: {props.currentTest.MemoryTest5}</li>) : (<li>Test 5: </li>)}
+              {props.currentTest.MemoryTest6 ? (<li>Test 6: {props.currentTest.MemoryTest6}</li>) : (<li>Test 6: </li>)}
+              {props.currentTest.MemoryTest7 ? (<li>Test 7: {props.currentTest.MemoryTest7}</li>) : (<li>Test 7: </li>)}
+              {props.currentTest.MemoryTest8 ? (<li>Test 8: {props.currentTest.MemoryTest8}</li>) : (<li>Test 8: </li>)}
+            </ul>
+          </div>;
+
+        
+      }
+
+    }
+    else if (props.currentRouterPath === '/Analysis') {
+      greeting = 'Analysis'
+    }
+    else if (props.currentRouterPath === '/DashBoard') {
+      greeting = 'DashBoard'
+    }
+
   }
-  else if(props.currentRouterPath === '/Test'){
-    greeting ='Test';
-      
-  }
-  else if(props.currentRouterPath === '/Analysis'){
-      greeting ='Analysis'
-  }
-  else if(props.currentRouterPath === '/DashBoard'){
-      greeting ='DashBoard'
-  }
-  return ( 
+  sideNavConditionalRender();
+  return (
     <div style={sideNavStyle}>
-      <br/>
+      <br />
       <h3 style={greetingStyle}>{greeting}</h3>
+      {content}
 
     </div>
   );
@@ -41,5 +84,5 @@ const mapStateToProps = (state) => ({
   currentTest: state.currentTest,
   testScriptPosition: state.testScriptPosition
 });
- 
+
 export default connect(mapStateToProps)(SideNav);
--- src/reducers/currentTestReducer.js
+++ src/reducers/currentTestReducer.js
@@ -2,20 +2,25 @@ import  Constants from './../constants';
 console.log(Constants);
 const { constants } = Constants;
 
-const currentTestReducer = (state = {}, action) => {
+const currentTestReducer = (state = '', action) => {
     let newState;
     switch(action.type){
         case constants.NEW_CURRENT_TEST:
             newState= Object.assign({}, action.test);
             return newState;
+
         case constants.ADD_REACTIONTIME_TEST_RESULT:
-            let reactiionTestObjectKey= "ReactionTimeTest "+ action.id;            newState= Object.assign({},state, { [reactiionTestObjectKey] : action.result})
+            let reactiionTestObjectKey= "ReactionTimeTest"+ action.id;
+            newState= Object.assign({},state, { [reactiionTestObjectKey] : action.result})
             console.log(newState);
             return newState;
+
         case constants.ADD_MEMORY_TEST_RESULT:
-            let memoryTestObjectKey = "MemoryTest "+ action.id;            newState = Object.assign({},state, { [memoryTestObjectKey] : action.result})
+            let memoryTestObjectKey = "MemoryTest"+ action.id;
+            newState = Object.assign({},state, { [memoryTestObjectKey] : action.result})
             console.log(newState);
             return newState;
+            
         default:
             return state;
     }
