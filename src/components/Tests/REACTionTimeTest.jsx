bb1d50f7e2330ad7af4b0324f38625df169b7b55
Devin Cooley <dcooley1350@gmail.com>
2019-12-14 16:25:40 -0800

    Write ReactionTimeTest component

--- src/components/Tests/REACTionTimeTest.jsx
+++ src/components/Tests/REACTionTimeTest.jsx
@@ -7,21 +7,45 @@ import { addReactionTimeResult } from '../../actions/currentTestActions';
 class REACTionTimeTest extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {  };
+        this.state = {
+            scriptPosition: 0,
+            iconPositions: [],
+            correctIcon: undefined,
+          };
+        this.setIconPositions();
         this.onAdvanceButtonClick=this.onAdvanceButtonClick.bind(this);
+        this.setIconPositions=this.setIconPositions.bind(this);
     };
+
+    componentDidUpdate(prevProps){
+        if(prevProps.iconPositions !== this.props.memoryItems){
+            this.setIconPositions();
+        }
+    }
+
     onAdvanceButtonClick(event){
     event.preventDefault();
        this.props.dispatch(advanceTestScript());
        this.props.dispatch(addReactionTimeResult("result",this.props.id))
     };
+
+    setIconPositions(){
+        let positions= [1,2,3,4,5,6,7,8,9,10,11,12];
+        function random(a, b) { return 0.5 - Math.random(); };
+        let randomIconPositions = positions.sort(random);
+        this.setState({ iconPositions: randomIconPositions })
+    }
     render() {
         const buttonStyle={
             marginBottom: '10px'
         };
+        const mainDivStyle = {
+            textAlign: 'center',
+            height: '100%'
+        }
         return (
-            <div>
-                <p>This is a reactionTimeTest. {this.props.id}</p>
+            <div style={mainDivStyle}>
+                <p>Reaction Time Test  #{this.props.id}</p>
                 <Button style={buttonStyle} color='primary' variant='outlined' onClick={this.onAdvanceButtonClick}>Next</Button>
             </div>
         );
