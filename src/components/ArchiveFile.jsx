795ea2b63b8c6c1e5bbdde348a7d0e4f66243b29
Student <example@example.com>
2019-12-18 13:18:58 -0800

    Write Archive file to display all data

--- src/components/ArchiveFile.jsx
+++ src/components/ArchiveFile.jsx
@@ -5,6 +5,7 @@ import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Grid from '@material-ui/core/Grid';
 
 const archiveFileStyles = makeStyles(theme => ({
     root: {
@@ -15,7 +16,7 @@ const archiveFileStyles = makeStyles(theme => ({
         flexBasis: '33.33%',
         flexShrink: 0
     },
-    sexondaryHeading: {
+    secondaryHeading: {
         fontSize: theme.typography.pxToRem(15),
         color: theme.palette.text.secondary,
     },
@@ -33,18 +34,91 @@ const ArchiveFile = (props) => {
             <ExpansionPanel expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
                 <ExpansionPanelSummary
                     expandIcon={<ExpandMoreIcon />} aria-controls="panel1bh-content" id="panel1bh-header">
-                    <Typography className={classes.heading}>{props.date}, {props.time}</Typography>
+                    <Typography className={classes.heading}>Date:{props.date}, Time: {props.time}</Typography>
                     <Typography className={classes.secondaryHeading}>Exp. Condition: {props.expCondition} </Typography>
                 </ExpansionPanelSummary>
                 <ExpansionPanelDetails>
-                    <Typography>Experimental Condition: {props.expConditionNotes}</Typography>
-                    <Typography>Control Condition: {props.ctrlCondition}</Typography>
-                    <Typography>Control Condition Notes: {props.ctrlConditionNotes}</Typography>
-                    <Typography>Control Condition Notes: {props.ctrlConditionNotes}</Typography>
+                    <div className={classes.root}>
+
+                    <Grid container>
+                        <Grid item xs={6}>
+                            <Typography>Experimental Condition Notes: {props.expConditionNotes}</Typography>
+                        </Grid>
+                        <Grid item xs={6}>
+                        <Typography>Control Condition: {props.ctrlCondition}</Typography>
+                        <Typography>Control Condition Notes: {props.ctrlConditionNotes}</Typography>
+                        </Grid>
+                    </Grid>
+                    <Grid container>
+                        <Grid item xs={6}>
+                        <h5>Memory Tests:</h5>
+                        <ol>
+                            <li>
+                                <ul>
+                                    <li>Input: {props.memoryTest1.userInputNumbersString}</li>
+                                    <li>Actual: {props.memoryTest1.correctNumberString}</li>
+                                </ul>
+                            </li>
+                            <li>
+                                <ul>
+                                    <li>Input: {props.memoryTest2.userInputNumbersString}</li>
+                                    <li>Actual: {props.memoryTest2.correctNumberString}</li>
+                                </ul>
+                            </li>
+                            <li>
+                                <ul>
+                                    <li>Input: {props.memoryTest3.userInputNumbersString}</li>
+                                    <li>Actual: {props.memoryTest3.correctNumberString}</li>
+                                </ul>
+                            </li>
+                            <li>
+                                <ul>
+                                    <li>Input: {props.memoryTest4.userInputNumbersString}</li>
+                                    <li>Actual: {props.memoryTest4.correctNumberString}</li>
+                                </ul>
+                            </li>
+                            <li>
+                                <ul>
+                                    <li>Input: {props.memoryTest5.userInputNumbersString}</li>
+                                    <li>Actual: {props.memoryTest5.correctNumberString}</li>
+                                </ul>
+                            </li>
+                            <li>
+                                <ul>
+                                    <li>Input: {props.memoryTest6.userInputNumbersString}</li>
+                                    <li>Actual: {props.memoryTest6.correctNumberString}</li>
+                                </ul>
+                            </li>
+                            <li>
+                                <ul>
+                                    <li>Input: {props.memoryTest7.userInputNumbersString}</li>
+                                    <li>Actual: {props.memoryTest7.correctNumberString}</li>
+                                </ul>
+                            </li>
+                            <li>
+                                <ul>
+                                    <li>Input: {props.memoryTest8.userInputNumbersString}</li>
+                                    <li>Actual: {props.memoryTest8.correctNumberString}</li>
+                                </ul>
+                            </li>
+                        </ol>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <h5>Reaction Time Tests:</h5>
+                            <ol>
+                                <li>Time: {props.reactionTimeTest1}ms</li>
+                                <li>Time: {props.reactionTimeTest2}ms</li>
+                                <li>Time: {props.reactionTimeTest3}ms</li>
+                                <li>Time: {props.reactionTimeTest4}ms</li>
+                                <li>Time: {props.reactionTimeTest5}ms</li>
+                            </ol>
+                        </Grid>
+                    </Grid>
+                    </div>
                 </ExpansionPanelDetails>
             </ExpansionPanel>
         </div>
-            );
-        }
-         
+    );
+}
+
 export default ArchiveFile;
